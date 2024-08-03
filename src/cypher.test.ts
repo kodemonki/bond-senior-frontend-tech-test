997b88655c8cf62c0811f9dacaff7ca78556019a
Tom Jane <kodemonki@hotmail.com>
2024-08-03 21:56:55 +0100

    clean up

--- src/cypher.test.ts
+++ src/cypher.test.ts
@@ -1,13 +1,12 @@
 import  onShift  from "./cypher";
 
 describe("bar", () => {
+  const str = "abcdefghijklmnopqrstuvwxyz";
   it("should shift right", () => {
-    const str = "abcdefghijklmnopqrstuvwxyz";
     const shift = 3;
     expect(onShift(str, shift)).toBe("defghijklmnopqrstuvwxyzbc");
   });
   it("should shift left", () => {
-    const str = "abcdefghijklmnopqrstuvwxyz";
     const shift = -3;
     expect(onShift(str, shift)).toBe("xyzabcdefghijklmnopqrstuvw");
   });
